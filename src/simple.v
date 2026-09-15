b187c3bf0dabf7a453be532d074f055c3758dfc1
llaammbbddaa <sarroufjohn@gmail.com>
2026-09-14 17:18:00 -0700

    case

--- src/simple.v
+++ src/simple.v
@@ -17,12 +17,21 @@
 //  If it passes, you will see a green check mark at the top of the repository.
 //
 
-module simple(
-	      input [2:0]  a,
-	      output reg [6:0] result);
+module simple(input [2:0]  a, output reg [6:0] result);
    
 // add your code here
-   
-endmodule
+	always @(a)
+		case(a)
+			0: result = 7'b0000000;
+                        1: result = 7'b0000001;
+                        2: result = 7'b0000011;
+                        3: result = 7'b0000111;
+                        4: result = 7'b0001111;
+                        5: result = 7'b0011111;
+                        6: result = 7'b0111111;
+                        7: result = 7'b1111111;
+		endcase
+
+endmodule // simple
 
 
